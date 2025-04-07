944bc7915ed796468fec6207a1d2037a9fed7e70
Nils <nilsjohan00@hotmail.com>
2025-04-07 16:10:50 +0200

    uppg. besk på Recording

--- src/main/java/se/su/ovning2/Recording.java
+++ src/main/java/se/su/ovning2/Recording.java
@@ -3,6 +3,18 @@ package se.su.ovning2;
 import java.util.Collection;
 import java.util.Set;
 
+//Klassen Recording representerar en inspelning (antingen CD eller LP). En skillnad mot motsvarande klass i Ovningsuppgift 1 ¨ ¨ar att Recording h¨ar ¨aven inneh˚aller en Set<String> genre, som inneh˚aller en m¨angd namn p˚a genrer (kan
+//vara flera), som denna inspelning representerar. Dessutom representeras LP respektive CD endast genom ett String-attribut, inte som olika subklasser.
+//Din uppgift ¨ar att modifiera klassen s˚a att dess objekt kan fungera v¨al som
+//nycklar i en avbildingstabell eller m¨angd (b˚ade hashade och tr¨adbaserade).
+//I samband med det ska likhet (equality) vara definierad s˚a att tv˚a olika objekt
+//av klassen ska betraktas som lika om de har samma titel, artist och ˚ar. Typ och
+//genre skall inte spela n˚agon roll. Egentligen har objekt av typen Recording ingen
+//naturlig sorteringsordning, men det ¨ar ok att definiera en sorteringsordning om
+//det kr¨avs f¨or att l¨osa n˚agon del av uppgiften. Det ¨ar ocks˚a ok att ist¨allet definiera
+//ordningen i varje ordnad datastruktur man vill anv¨anda, eller med en blandning
+//av de tv˚a s¨atten.
+
 public class Recording {
   private final int year;
   private final String artist;
